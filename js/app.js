c49b54887818e7eb55f525238f2a37e561805982
Your Name <you@example.com>
2022-11-29 20:26:27 +0100

    register

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-
+let b_image = document.getElementById('b_image')
+let s_image = document.getElementById('s_image').getElementsByTagName('img')
 
 let center = document.querySelector('.center')
 let bug = document.querySelector('.bug')
@@ -8,3 +9,21 @@ bug.addEventListener('click',()=>{
     center.classList.toggle('active');
 })
 
+
+
+for(let i=0; i < s_image.length; i++){
+    s_image[i].addEventListener('click', full_image)
+}
+
+function full_image(){
+    let imageSRC = this.getAttribute('src')
+    b_image.innerHTML = "<img src=" + imageSRC + " width='400' height='400'>"
+}
+
+     
+    // for(let i =0; i<4; i++){
+    //      smallImg[i].onclick = function(){
+    //      mainImg.src = smallImg[i].src;
+    // }
+ 
+    // }
